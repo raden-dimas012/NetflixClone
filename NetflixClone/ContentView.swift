39dae076bfe86d8ffefee8f5d5ff731c4d3bb2e3
raden-dimas012 <dimascode012@gmail.com>
2022-04-03 14:52:14 +0700

    Initial Commit

---
+++ NetflixClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetflixClone
+//
+//  Created by Raden Dimas on 03/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Raden Dimas on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
